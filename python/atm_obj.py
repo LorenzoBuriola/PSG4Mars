5d0046d9332ed16eb8711712114789c7ea8f0f08
Lorenzo Buriola <lorenzo.buriola@unibo.it>
2024-07-09 11:14:18 +0200

    gas object and HITRAN table

---
+++ python/atm_obj.py
@@ -0,0 +1,22 @@
+from pandas import read_csv
+from warnings import warn
+
+class gas:
+    def __init__(self, name, abun, unit) -> None:
+        self.name = name
+        self.abun = abun
+        self.unit = unit
+        self.code = None
+        self.code = self.get_HITRAN_code()
+    
+    def get_HITRAN_code(self):
+        if self.code is not None:
+            return self.code
+        else:
+            tab = read_csv('molecular_metadata.txt', sep = '\s+')
+            code = tab[tab.Formula == self.name]['Molecule_ID']
+            if not code.empty:
+                return code.iloc[0]
+            else:
+                warn(f'{self.name} is not a HITRAN molecule')
+                return None
