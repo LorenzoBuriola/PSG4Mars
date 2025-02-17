85d80911c90a1b50c1e862945d67b48190b55e32
Lorenzo Buriola <lorenzo.buriola@unibo.it>
2025-02-17 14:36:59 +0100

    17 febbraio

--- src/generate_OD.py
+++ src/generate_OD.py
@@ -10,7 +10,7 @@ def T_shift(DT, cfg_df):
     cfg_out['SURFACE-TEMPERATURE'] = df.loc[0,'Temperature']
     return cfg_out
 
-gas_list = ['CO2','CO','H2O','O3','HDO','HCl']
+gas_list = ['CO2', 'H2O', 'CO', 'O3', 'HCl', 'HDO']
 cfg_path = '/home/buriola/PSG/PSG4Mars/data/cfg/'
 lyo_path = '/home/buriola/PSG/PSG4Mars/data/lyo/'
 lyr_path = '/home/buriola/PSG/PSG4Mars/data/lyr/'
@@ -27,10 +27,17 @@ for g_name in gas_list:
         print(f'Temperature shift: {DT}')
         temp = T_shift(DT, cfg_dict)
         for i in range(len(ranges)-1):
-            temp['GENERATOR-RANGE1'] = ranges[i]+1e-4
+            temp['GENERATOR-RANGE1'] = ranges[i]
             temp['GENERATOR-RANGE2'] = ranges[i+1]
+            print(f'freqs: {ranges[i]}-{ranges[i+1]}')
             cfg.dict_to_cfg(temp, f'{cfg_path}/OD_gen/cfg_temp.txt')
-            run_psg(cfg_file=f'{cfg_path}/OD_gen/cfg_temp.txt', type='lyo', out_file=f"{lyo_path}{g_name}/lyo_{g_name}_{DT}_freq{ranges[i]}_{ranges[i+1]}.txt", verbose=False)
-            run_psg(cfg_file=f'{cfg_path}/OD_gen/cfg_temp.txt', type='lyr', out_file=f"{lyr_path}{g_name}/lyr_{g_name}_{DT}_freq{ranges[i]}_{ranges[i+1]}.txt", verbose=False)
+            run_psg(cfg_file=f'{cfg_path}/OD_gen/cfg_temp.txt',
+                        type='lyo',
+                        out_file=f"{lyo_path}{g_name}/lyo_{g_name}_{DT}_freq{ranges[i]}_{ranges[i+1]}.txt",
+                        verbose=False)
+            run_psg(cfg_file=f'{cfg_path}/OD_gen/cfg_temp.txt',
+                        type='lyr',
+                        out_file=f"{lyr_path}{g_name}/lyr_{g_name}_{DT}_freq{ranges[i]}_{ranges[i+1]}.txt",
+                        verbose=False)
 
-print("That's all!")
+print("\nThat's all!")
