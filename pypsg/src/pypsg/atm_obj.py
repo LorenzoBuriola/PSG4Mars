2e51b926d839552dacc81fe8a93f31ec38cb280c
Lorenzo Buriola <lorenzo.buriola@unibo.it>
2025-04-28 12:25:34 +0200

    no more metatdata file

--- pypsg/src/pypsg/atm_obj.py
+++ pypsg/src/pypsg/atm_obj.py
@@ -3,6 +3,18 @@
 import pandas as pd
 from warnings import warn
 
+molecular_metadata = {'H2O': 1, 'CO2': 2, 'O3': 3, 'N2O': 4, 'CO': 5,
+                'CH4': 6, 'O2': 7, 'NO': 8, 'SO2': 9, 'NO2': 10,
+                'NH3': 11, 'HNO3': 12, 'OH': 13, 'HF': 14, 'HCl': 15,
+                'HBr': 16, 'HI': 17, 'ClO': 18, 'OCS': 19, 'H2CO': 20,
+                'HOCl': 21, 'N2': 22, 'HCN': 23, 'CH3Cl': 24, 'H2O2': 25,
+                'C2H2': 26, 'C2H6': 27, 'PH3': 28, 'COF2': 29, 'SF6': 30,
+                'H2S': 31, 'HCOOH': 32, 'HO2': 33, 'O': 34, 'ClONO2': 35,
+                'NO+': 36, 'HOBr': 37, 'C2H4': 38, 'CH3OH': 39, 'CH3Br': 40,
+                'CH3CN': 41, 'CF4': 42, 'C4H2': 43, 'HC3N': 44, 'H2': 45,
+                'CS': 46, 'SO3': 47, 'C2N2': 48, 'COCl2': 49, 'SO': 50,
+                'CH3F': 51, 'GeH4': 52, 'CS2': 53, 'CH3I': 54, 'NF3': 55}
+
 class gas:
     def __init__(self, name, abun = '1', unit = 'scl') -> None:
         self.code = None
@@ -22,13 +34,8 @@ class gas:
         if self.code is not None:
             return self.code
         else:
-            tab = pd.read_csv('molecular_metadata.txt', sep = '\\s+')
-            code = tab[tab.Formula == self.name]['Molecule_ID']
-            if not code.empty:
-                return code.iloc[0]
-            else:
-                warn(f'{self.name} is not a HITRAN molecule')
-                return None
+            code = molecular_metadata[self.name]
+            return code
             
 class aeros:
     def __init__(self, name, abun = '1', unit = 'scl', size = '1', sunit = 'scl', type='CRISM_Wolff') -> None:
